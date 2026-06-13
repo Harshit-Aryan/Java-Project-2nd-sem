1e94cb89c4efbdc60e6c386cd54021aac1807371
Harshit Aryan <harshitjarh10@gmail.com>
2026-06-13 20:50:40 +0530

    Update FullTime.java

--- FullTime.java
+++ FullTime.java
@@ -1,21 +1,19 @@
-// Subclass for full-time employees
-// Salary = baseSalary + bonus - taxDeduction
+
 public class FullTime extends Employee {
 
     private double baseSalary;
     private double bonus;
     private double taxDeduction;
 
-    // Parameterized constructor using super()
     public FullTime(String name, int employeeId, String department,
                     double baseSalary, double bonus, double taxDeduction) {
-        super(name, employeeId, department);   // call parent constructor
+        super(name, employeeId, department);  
         this.baseSalary    = baseSalary;
         this.bonus         = bonus;
         this.taxDeduction  = taxDeduction;
     }
 
-    // Polymorphic salary calculation for full-time employees
+
     @Override
     public double calculateSalary() {
         return baseSalary + bonus - taxDeduction;
